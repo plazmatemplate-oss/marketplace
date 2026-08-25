b9fced90d3ce28324fe9e8a0033d75248c3380d2
plazmatemplate-oss <plazmatemplate@gmail.com>
2026-08-25 11:34:33 +0530

    add: scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -10,6 +11,7 @@ import FloatingSocial from "@/components/layout/FloatingSocial";
 import BackToTop from "@/components/common/BackToTop";
 import WhatsAppButton from "@/components/common/WhatsAppButton";
 import Providers from "./providers";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -43,9 +45,6 @@ export const metadata: Metadata = {
   },
 };
 
-import { Toaster } from "@/components/ui/sonner";
-import Script from "next/script";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -58,11 +57,19 @@ export default function RootLayout({
       className={cn("antialiased font-sans", inter.variable)}
     >
       <head>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3763297443777958"
-          crossOrigin="anonymous"></Script>
-        <meta name="google-adsense-account" content="ca-pub-3763297443777958"></meta>
+        <meta name="google-adsense-account" content="ca-pub-3763297443777958" />
       </head>
       <body className="flex flex-col min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMKDMG88"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Providers>
           <Toaster />
           <div className="hidden md:block">
@@ -77,6 +84,69 @@ export default function RootLayout({
           <BackToTop />
           <WhatsAppButton />
         </Providers>
+
+        {/* Google AdSense */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3763297443777958"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MMKDMG88');`,
+          }}
+        />
+
+        {/* Google Ads Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10778899030"
+          strategy="afterInteractive"
+        />
+
+        {/* Google Analytics 4 Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SG9TG65S2M"
+          strategy="afterInteractive"
+        />
+
+        {/* Gtag Config */}
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-10778899030');
+              gtag('config', 'G-SG9TG65S2M');
+            `,
+          }}
+        />
+
+        {/* Tawk.to Live Chat Script */}
+        <Script
+          id="tawk-to"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.defer=true;
+              s1.src='https://embed.tawk.to/6773754949e2fd8dfe00d91a/1igdg4ahk';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
